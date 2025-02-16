5789d3a676218c6a0ab0f7d0df828be6e291d694
Fatima Dayanna Villanueva Flores <dayannavillanueva452@gmail.com>
2025-02-15 22:36:33 -0500

    feat: employee list and employee card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FiltersComponent } from './components/blocks/filters/filters.component';
+import { EmploymentListComponent } from './components/contents/employment-list/employment-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FiltersComponent],
+  imports: [RouterOutlet, FiltersComponent, EmploymentListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/components/contents/employment-list/employment-list.component.ts
+++ src/app/components/contents/employment-list/employment-list.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { EmploymentCardComponent } from '../../blocks/employment-card/employment-card.component';
 
 @Component({
   selector: 'app-employment-list',
-  imports: [],
+  imports: [EmploymentCardComponent],
   templateUrl: './employment-list.component.html',
-  styleUrl: './employment-list.component.css'
+  styleUrl: './employment-list.component.css',
 })
-export class EmploymentListComponent {
-
-}
+export class EmploymentListComponent {}
